af64de9332361f9b5bbd8f85600f3fae05297c70
Hacker6329 <Hacker6329@gmail.com>
2026-01-04 21:25:50 +0100

    Added a UUIDGenerator because why not

---
+++ src/main/java/it/italiandudes/despicable_cards/UUIDGenerator.java
@@ -0,0 +1,9 @@
+package it.italiandudes.despicable_cards;
+
+import java.util.UUID;
+
+public class UUIDGenerator {
+    public static void main(String[] args) {
+        for (int i=0; i<100; i++) System.out.println(UUID.randomUUID());
+    }
+}

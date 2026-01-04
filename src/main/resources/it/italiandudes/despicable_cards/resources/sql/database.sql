896b22810a55adb04f893b7a40ba24a12b304beb
Hacker6329 <Hacker6329@gmail.com>
2026-01-04 03:32:10 +0100

    Updated protocol example and db query

--- src/main/resources/it/italiandudes/despicable_cards/resources/sql/database.sql
+++ src/main/resources/it/italiandudes/despicable_cards/resources/sql/database.sql
@@ -3,3 +3,19 @@ CREATE TABLE IF NOT EXISTS key_parameters (
     param_key VARCHAR(32) NOT NULL PRIMARY KEY,
     param_value TEXT
 );
+
+-- TABLE: White Cards
+CREATE TABLE IF NOT EXISTS whitecards (
+    id INTEGER NOT NULL PRIMARY KEY,
+    uuid TEXT NOT NULL UNIQUE,
+    content TEXT NOT NULL,
+    is_wildcard INTEGER NOT NULL DEFAULT 0
+);
+
+-- TABLE: Black Cards
+CREATE TABLE IF NOT EXISTS blackcards (
+    id INTEGER NOT NULL PRIMARY KEY,
+    uuid TEXT NOT NULL UNIQUE,
+    content TEXT NOT NULL,
+    blanks INTEGER NOT NULL
+);

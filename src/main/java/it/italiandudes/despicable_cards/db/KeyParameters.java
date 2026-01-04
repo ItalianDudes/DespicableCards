fe3985cdf00d54c2deea00f43765f02439b89140
Hacker6329 <Hacker6329@gmail.com>
2026-01-04 03:31:48 +0100

    Added db title

--- src/main/java/it/italiandudes/despicable_cards/db/KeyParameters.java
+++ src/main/java/it/italiandudes/despicable_cards/db/KeyParameters.java
@@ -2,4 +2,5 @@ package it.italiandudes.despicable_cards.db;
 
 public final class KeyParameters {
     public static final String DB_VERSION = "db_version";
+    public static final String DB_TITLE = "db_title";
 }

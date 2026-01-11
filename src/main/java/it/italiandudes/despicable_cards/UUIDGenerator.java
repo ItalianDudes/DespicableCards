2b96e200d9c76ffc0b502952efc41d769a37281c
Hacker6329 <Hacker6329@gmail.com>
2026-01-11 20:22:20 +0100

    Added arg for gen 100 UUIDS

--- src/main/java/it/italiandudes/despicable_cards/UUIDGenerator.java
+++ src/main/java/it/italiandudes/despicable_cards/UUIDGenerator.java
@@ -2,7 +2,7 @@ package it.italiandudes.despicable_cards;
 
 import java.util.UUID;
 
-public class UUIDGenerator {
+public final class UUIDGenerator {
     public static void main(String[] args) {
         for (int i=0; i<100; i++) System.out.println(UUID.randomUUID());
         System.exit(0);
